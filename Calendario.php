ebb5bb0437f4a36bce9ce2c384dfc076e2324e37
EricPerLuq <eperezluque@iesesteveterradas.cat>
2018-09-28 17:00:00 +0200

    first commit

---
+++ Calendario.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calendario</title>
+	<h1>Calendario</h1>
+	<link rel="stylesheet" type="text/css" href="Calendario.css">
+</head>
+<body>
+<table>
+	<tr>
+		<td>Dilluns</td>
+		<td>Dimarts</td>
+		<td>Dimecres</td>
+		<td>Dijous</td>
+		<td>Divendres</td>
+		<td>Dissabte</td>
+		<td>Diumenge</td>
+	</tr>
+	<?php
+	$filas=6;
+	$columnas=7;
+	$num=1;
+	$pos=1;
+		
+		for($i=0; $i<$filas ;$i++){
+			echo "<tr>";
+
+			for($a=0; $a <$columnas; $a++){
+			
+				if ($pos==5) {
+					if($num<=31){
+				
+					echo "<td>$num</td>";
+					$num+=1;
+					}
+					
+				}else{
+					echo "<td></td>";
+						$pos++;}
+				
+			}
+		
+			echo "</tr>";
+		}
+		 
+
+	?>
+</table>
+</body>
+</html>
+
